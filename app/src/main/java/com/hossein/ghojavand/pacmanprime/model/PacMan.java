ceadbfa2003652efd06304125ffdd8b6e0a9add3
hossein.ghojavand.27 <hossein.ghojavand.27@gmail.com>
2020-11-13 22:58:09 +0330

    again

---
+++ app/src/main/java/com/hossein/ghojavand/pacmanprime/model/PacMan.java
@@ -0,0 +1,14 @@
+package com.hossein.ghojavand.pacmanprime.model;
+
+public class PacMan {
+    public int iPosition;
+    public int jPosition;
+    public int score;
+
+    public PacMan(int iPosition , int jPosition)
+    {
+        this.iPosition = iPosition;
+        this.jPosition = jPosition;
+        score = 0;
+    }
+}
